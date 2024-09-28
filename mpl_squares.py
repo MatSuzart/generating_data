ad19d3ad18f3a48a54ecbf0edf40558a85fd7e8c
Matheus Suzart <mathsuzart@outlook.com>
2024-09-27 23:07:54 -0300

    eixos

--- mpl_squares.py
+++ mpl_squares.py
@@ -3,5 +3,15 @@ import matplotlib.pyplot as plt
 squares = [1,4,9,16,25]
 
 fig, ax = plt.subplots()
+ax.plot(squares, linewidth=3)
+
+#Define o Título do gráfico e os eixos do rótulo
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value",fontsize=14)
+
+#Define o tamanho dos rótulos de marcação de escala
+ax.tick_params(labelsize=14)
+
 ax.plot(squares)
 plt.show()
