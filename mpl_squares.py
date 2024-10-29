6f257d0d36b94a7b12ef46b7c54d007b128715a7
Matheus Suzart <mathsuzart@outlook.com>
2024-10-29 00:06:40 -0300

    fix

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,23 +1,21 @@
 import matplotlib.pyplot as plt
-plt.style.available
 
-input_values = [1,2,3,4,5]
-squares = [1,4,9,16,25]
+print(plt.style.available)
 
-plt.style.use('seaboarn')
+input_values = [1, 2, 3, 4, 5]
+squares = [1, 4, 9, 16, 25]
+
+plt.style.use('seaborn')
 
 fig, ax = plt.subplots()
+ax.plot(input_values, squares, linewidth=3)
 
-ax.plot(input_values,squares,linewidth=3)
-ax.plot(squares, linewidth=3)
-
-#Define o Título do gráfico e os eixos do rótulo
+# Define o Título do gráfico e os eixos do rótulo
 ax.set_title("Square Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
-ax.set_ylabel("Square of Value",fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
 
-#Define o tamanho dos rótulos de marcação de escala
+# Define o tamanho dos rótulos de marcação de escala
 ax.tick_params(labelsize=14)
 
-ax.plot(squares)
 plt.show()
