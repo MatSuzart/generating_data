5b422c168d40023daeae21ca8e35dbbf98d8d4f8
Matheus Suzart <mathsuzart@outlook.com>
2024-11-12 00:34:33 -0300

    fix: random_walk

--- random_walk.py
+++ random_walk.py
@@ -31,3 +31,6 @@ class RandomWalk:
             # Calcula a nova posição
             x = self.x_values[-1] + x_step
             y = self.y_values[-1] + y_step
+
+            self.x_values.append(x)
+            self.y_values.append(y)
