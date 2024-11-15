42f3b1789b440467e06fad62524b57cc95083787
Matheus Suzart <mathsuzart@outlook.com>
2024-11-15 18:10:45 -0300

    :recycle:feat - rw_visual

--- rw_visual.py
+++ rw_visual.py
@@ -2,9 +2,28 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-#Cria um random walk
-rw = RandomWalk()
-rw.fill_walk()
+while True:
+    #Cria um random walk
+    rw = RandomWalk(50_000)
+    rw.fill_walk()
 
-#Plota os pontos no passeio
-plt.style.use('classic')
+    #Plota os pontos no passeio
+    plt.style.use('classic')
+    fig,ax = plt.subplots(10,6,dpi=128)
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values,rw.y_values,c=point_numbers,cmap=plt.cm.Blues,edgecolors='none',s=1)
+    ax.scatter(rw.x_values, rw.y_values, s=15)
+    ax.set_aspect('equal')
+    
+    #Destaca o primeiro e o último ponto
+    ax.scatter(0,0,c='green',edgecolors='none',s=100)
+    ax.scatter(rw.x_values[-1],rw.y_values[-1],c='red',edgecolors='none',s=100)
+    
+    #remove os eixos
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False) 
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n):")
+    if keep_running =='n':
+        break
